9fa5cfe56e6ecc9ea0e85366f87d9ca3b2640f62
Cactus jack <126608888+BBG0007@users.noreply.github.com>
2023-03-05 10:47:08 +0000

    New chnages

--- app.py
+++ app.py
@@ -2,41 +2,38 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data analyst',
-    'location': 'lagos, Nigeria',
-    'salary': 'N.500,000'
-  },
-  {
-    'id': 2,
-    'title': 'web developer',
-    'location': 'Abuja, Nigeria',
-    'salary': 'N.800,000'
-  },
- {
+JOBS = [{
+  'id': 1,
+  'title': 'Data analyst',
+  'location': 'lagos, Nigeria',
+  'salary': 'N.500,000'
+}, {
+  'id': 2,
+  'title': 'web developer',
+  'location': 'Abuja, Nigeria',
+  'salary': 'N.800,000'
+}, {
   'id': 3,
-    'title': 'programmer',
-    'location': 'lagos, Nigeria',
-    'salary': 'N.400,000'
- },
- {
-   'id': 4,
-    'title': 'backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$1,000,000'
- } 
-]
+  'title': 'programmer',
+  'location': 'lagos, Nigeria',
+  'salary': 'N.400,000'
+}, {
+  'id': 4,
+  'title': 'backend Engineer',
+  'location': 'San Francisco, USA',
+  'salary': '$1,000,000'
+}]
+
 
 @app.route("/")
 def hello_techboys():
-  return render_template('home.html',
-                         jobs=JOBS,
-                        company_name='techboys')
+  return render_template('home.html', jobs=JOBS, company_name='techboys')
+
+
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(jobs)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
